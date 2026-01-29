b21391529edbe5875c24f7361fb082d8429fb75c
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-01-29 19:44:03 +0100

    Guard for X86 ASM

--- src/data/cpu.cpp
+++ src/data/cpu.cpp
@@ -29,13 +29,15 @@
 void CPU::native_cpuid(unsigned int *eax, unsigned int *ebx,
         unsigned int *ecx, unsigned int *edx)
 {
-    /* ecx is often an input as well as an output. */
-    asm volatile("cpuid"
-    : "=a" (*eax),
-      "=b" (*ebx),
-      "=c" (*ecx),
-      "=d" (*edx)
-    : "0" (*eax), "2" (*ecx));
+    #if defined(__i386__) || defined(__x86_64__)
+        /* ecx is often an input as well as an output. */
+        asm volatile("cpuid"
+        : "=a" (*eax),
+        "=b" (*ebx),
+        "=c" (*ecx),
+        "=d" (*edx)
+        : "0" (*eax), "2" (*ecx));
+    #endif
 }
 
 void CPU::updateMemoryInfo() {
